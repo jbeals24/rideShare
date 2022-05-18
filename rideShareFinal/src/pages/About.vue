1d1d6ec0c396d345045a29b5baf34a08666f07a6
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2022-05-17 21:55:36 -0400

    Fix punctuation issue in About page

--- rideShareFinal/src/pages/About.vue
+++ rideShareFinal/src/pages/About.vue
@@ -3,8 +3,14 @@
     <div>
       <h4 class="display-1">About Us</h4>
 
-      <p class="body-1">We are a company built on assisting our clients to go from coast to coast. </p>
-      <p class= bosy-1"> Our ultimate goal is to get you to the places you need to go with no stress! </p>
+      <p class="body-1">
+        We are a company built on assisting our clients to go from coast to
+        coast.
+      </p>
+      <p class="body-1">
+        Our ultimate goal is to get you to the places you need to go with no
+        stress!
+      </p>
     </div>
   </v-container>
 </template>
